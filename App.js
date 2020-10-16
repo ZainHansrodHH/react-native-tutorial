940997833d4d0686b2bd3fc6fdc0484fdc87285b
ZainHansrodHH <zainhansrod786@gmail.com>
2020-10-15 21:43:21 -0400

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Cat name="Fluffi boi"></Cat>
     </View>
   );
 }
@@ -19,3 +20,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const testFunction = (text) => {
+  return text + '.'
+}
+
+const Cat = (props) => {
+  //You can add any JS within curly brackets
+  var name = "Zain";
+  return(
+    <ScrollView>
+      <Text>Hi, I am your first component. Welcome, {testFunction(name)}</Text>
+      <Text>Here's your name prop: {props.name}</Text>
+    </ScrollView>
+  );
+}
