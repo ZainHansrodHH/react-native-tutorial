bbdb935217636c9072256ee2885f40cc29d360a5
ZainHansrodHH <zainhansrod786@gmail.com>
2020-10-15 23:01:16 -0400

    Added component with Text input

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Text>React Basics</Text>
+      <Text>Open up App.js to start working on your app!!</Text>
       <StatusBar style="auto" />
       <Cat name="Fluffi boi"></Cat>
+      <Text>Handling Text Input</Text>
+      <PizzaTranslate></PizzaTranslate>
     </View>
   );
 }
 
+//Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,10 +25,15 @@ const styles = StyleSheet.create({
   },
 });
 
+//A function that can be created and used
 const testFunction = (text) => {
   return text + '.'
 }
 
+//A component (Which happens to be a custom one)
+//Props - Variables passed to our component when its being used.
+//Use state for variables that are going to change overtime
+//Any code added between the {} is JS and can be executed or ran
 const Cat = (props) => {
   //You can add any JS within curly brackets
   var name = "Zain";
@@ -34,4 +43,27 @@ const Cat = (props) => {
       <Text>Here's your name prop: {props.name}</Text>
     </ScrollView>
   );
+}
+
+//Example of handling user input
+//Vars outside of return statement
+const PizzaTranslate = (props) => {
+  const [text, setText] = useState('');
+  return(
+    <View>
+      <TextInput onChangeText={text => setText(text)}></TextInput>
+      <Text>{text.split(' ').map((word) => word && '🍕').join(' ')}</Text>
+    </View>
+  )
+}
+
+//Helper function for pizza translator
+const wordsToPizza = (text) => {
+  var wordSplit = text.split(' ');
+  var pizzaWord = "";
+  for (var i=0;wordSplit.length;i++){
+    wordSplit[i] = '🍕';
+    pizzaWord += wordSplit[i];
+  }
+  return pizzaWord;
 }
